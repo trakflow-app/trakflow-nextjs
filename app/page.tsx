ecda680a4e92114ba41e90129b36c7d72d16a8a5
Cindy Mae Ngoho <cindymaen17@gmail.com>
2026-04-15 17:46:07 -0400

    removed the middleware

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
               height={16}
             />
             Deploy Now
-          </a>
+          </Link>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -71,7 +71,7 @@ export default function Home() {
             placeholder="Select an option"
           />
         </div>
-      </div>
+      </main>
     </div>
   );
 }
