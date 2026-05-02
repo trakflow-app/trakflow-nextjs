7e2dc0be83612402692120b060c24ee31bab6832
Lisa Copeland <100307682+lisacopelandswe@users.noreply.github.com>
2026-05-01 20:09:05 -0400

    UI/KAN-47: Create Header Component (#33)
    * ui: created header component
    
    * fix: styling issues
    
    * fix: prettier errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/sonner';
 import { Footer } from '@/components/layout/footer';
+import { Header } from '@/components/layout/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,6 +31,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Header />
         {children}
         <Footer />
         <Toaster />
