27d5ca12304c18c63abc70d9b3a0b91eef9a8467
Lisa Copeland <lisacopelandswe@gmail.com>
2026-07-22 13:23:58 -0400

    ui/created custom 404 page

---
+++ constants/app/not-found-page-constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Destination for the public home page.
+ */
+export const NOT_FOUND_HOME_HREF = '/';
+
+/**
+ * Destination for the standard login route.
+ */
+export const NOT_FOUND_LOGIN_HREF = '/login';
